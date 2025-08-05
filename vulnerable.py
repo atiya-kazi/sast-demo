db973034019d0a3fb25c8a59ea5c5766d7e47966
atiya-kazi <107027835+atiya-kazi@users.noreply.github.com>
2025-08-05 10:27:43 +0530

    Update vulnerable.py

--- vulnerable.py
+++ vulnerable.py
@@ -1,5 +1,5 @@
+# trigger sonarcloud scan
 import sqlite3
-
 user_input = input("Enter username: ")
 query = "SELECT * FROM users WHERE username = '" + user_input + "'"  # SQL Injection vulnerability
 conn = sqlite3.connect("users.db")
