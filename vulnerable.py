1e42e960f3651ff654942f5297bcad1b53fd35c1
atiya-kazi <107027835+atiya-kazi@users.noreply.github.com>
2025-08-05 10:08:26 +0530

    Add vulnerable example

---
+++ vulnerable.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+user_input = input("Enter username: ")
+query = "SELECT * FROM users WHERE username = '" + user_input + "'"  # SQL Injection vulnerability
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
+cursor.execute(query)
+print(cursor.fetchall())
